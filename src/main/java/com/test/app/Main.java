806fbafb1c68910117d086c204229ceb2bd5b093
razikdontcare <gamersia4@gmail.com>
2024-11-06 11:04:32 +0800

    Remove hardcoded database password for improved security

--- src/main/java/com/test/app/Main.java
+++ src/main/java/com/test/app/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/test";
         String username = "root";
-        String password = "Aji123";
+        String password = "";
 
         Connection conn = null;
         Statement stmt = null;
